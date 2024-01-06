291ce6b23d70f00adc837a1284721a372910d59c
CEGROH <chrisgefferie@gmail.com>
2024-01-06 17:54:50 +0100

    end points

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
-# from flask_jwt_extended import JWTManager
+from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from db import DB
-from resources.user import test, create_teacher, create_user, get_user, get_specific_notes, create_notes
+from dotenv import load_dotenv
+import os
+from resources.user import test, create_teacher, create_user, get_user, get_specific_notes, create_question, get_notes, get_teachers, get_specific_teacher, user_login, me, teacher_login,insert_question, get_questions
 app = Flask(__name__)
 app.debug = True
 
@@ -10,22 +12,27 @@ app.debug = True
 CORS(app)
 
 # Register the JWT manager
-#app.config['JWT_SECRET_KEY'] = 'super-secret' # Change this!
-#jwt = JWTManager(app)
+app.config['JWT_SECRET_KEY'] = os.getenv("JWT_TOKEN_SECRET") # Change this!
+jwt = JWTManager(app)
 # ============================ Routes ============================
 
 # JWT routes
 app.add_url_rule('/test', None, test, methods=['GET'])
 app.add_url_rule('/get_user', None, get_user, methods=['GET'])
 app.add_url_rule('/get_specific_notes/<int:note_id>', None, get_specific_notes, methods=['GET'])
+app.add_url_rule('/get_notes/<int:note_id>', None, get_notes, methods=['GET'])
+app.add_url_rule('/get_teachers', None, get_teachers, methods=['GET'])
+app.add_url_rule('/get_specific_teachers/<int:teacher_id>', None, get_specific_teacher, methods=['GET'])
+app.add_url_rule('/get_questions', None, get_questions, methods=['GET'])
+app.add_url_rule('/me', None, me, methods=['GET'])
 app.add_url_rule('/create_teacher', None, create_teacher, methods=['POST'])
 app.add_url_rule('/create_user', None, create_user, methods=['POST'])
-app.add_url_rule('/create_notes', None, create_notes, methods=['POST'])
+app.add_url_rule('/create_question', None, create_question, methods=['POST'])
+app.add_url_rule('/user_login', None, user_login, methods=['POST'])
+app.add_url_rule('/teacher_login', None, teacher_login, methods=['POST'])
+app.add_url_rule('/insert_question', None, insert_question, methods=['POST'])
 
-
-
-
-
+# ============================ Main ============================
 
 # Start app
 if __name__ == '__main__':
--- resources/user.py
+++ resources/user.py
@@ -2,12 +2,22 @@ from flask import request
 from flask_bcrypt import generate_password_hash
 from db import DB
 from datetime import datetime
+from flask import Flask, jsonify
+
 from lib.testgpt.testgpt import TestGPT, FakeTestGPT
 import string
 import openai
 from dotenv import load_dotenv
 import os
 from openai import RateLimitError
+from flask_bcrypt import check_password_hash
+from flask_jwt_extended import (
+    jwt_required,
+    create_access_token,
+    get_jwt_identity
+)
+
+
 import random # define the random module
 
 # from flask_jwt_extended import (
@@ -19,6 +29,52 @@ from datetime import date
 
 def test():
     return{'message':'start server'}, 201 
+
+def teacher_login():
+        # Get data from request
+    display_name = request.json.get('display_name', None)
+    teacher_password = request.json.get('teacher_password', None)
+
+    # Get user from database
+    qry = 'SELECT * FROM `teachers` WHERE `display_name` = :display_name'
+    user = DB.one(qry, {'display_name': display_name})
+
+    # Check if user exists and password is correct
+    if not user or not check_password_hash(user['teacher_password'], teacher_password):
+        return {'message': 'invalid credentials'}, 401
+    
+    # Delete password from user (should not be sent back!)
+    del user['teacher_password']
+
+    # Create JWT
+    access_token = create_access_token(user)
+    return jsonify(access_token = access_token, message = 'success'), 200
+
+def user_login():
+    # Get data from request
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+
+    # Get user from database
+    qry = 'SELECT * FROM `users` WHERE `email` = :email'
+    user = DB.one(qry, {'email': email})
+
+    # Check if user exists and password is correct
+    if not user or not check_password_hash(user['password'], password):
+        return {'message': 'invalid credentials'}, 401
+    
+    # Delete password from user (should not be sent back!)
+    del user['password']
+
+    # Create JWT
+    access_token = create_access_token(user)
+    return jsonify(access_token = access_token, message = 'success'), 200
+    
+
+@jwt_required()
+def me():
+    user = get_jwt_identity()
+    return jsonify(user=user, message='success'), 200
 
 def create_teacher():
     args = request.json
@@ -43,6 +99,30 @@ def create_teacher():
         print('Er is een probleem opgetreden, contact de admin.')
 
     return {'message': 'success', 'id': id}, 201
+
+def get_teachers():
+    qry = '''
+    SELECT * FROM teachers
+    '''
+   
+    try:
+        rows = DB.all(qry)
+    except Exception:
+        print('Er is een probleem opgetreden, contact de admin.')
+
+    return {'message': 'success', 'rows': rows}, 201
+
+def get_specific_teacher(teacher_id):
+    qry = '''
+    SELECT * FROM teachers WHERE teacher_id = ?
+    '''
+   
+    try:
+        rows = DB.all(qry, (teacher_id,))
+    except Exception:
+        print('Er is een probleem opgetreden, contact de admin.')
+
+    return {'message': 'success', 'rows': rows}, 201
 
 def create_user():
     args = request.json
@@ -83,11 +163,72 @@ def get_user():
 def get_specific_notes(note_id):
     
     qry = '''
-    SELECT notes.note_id , notes.title, notes.note_source, notes.is_public, teachers.display_name, notes.category_id, notes.date_created
+    SELECT notes.note_id , notes.title, notes.note_source, notes.is_public, teachers.display_name, notes.category_id,  categories.omschrijving, notes.date_created
     FROM notes
     INNER JOIN teachers 
     on notes.teacher_id = teachers.teacher_id
+    INNER JOIN categories 
+    ON notes.category_id= categories.category_id
+
+    WHERE note_id =?
+	
+    '''
+
+    try:
+        rows = DB.all(qry, (note_id,))
+    except Exception:
+        print('Er is een probleem opgetreden, contact de admin.')
+
+    return {'message': 'success', 'rows': rows}, 201
+
+def insert_question():
+    args = request.json
+
+    qry = '''
+    INSERT INTO
+        `questions`
+            (`note_id`, `exam_question`, `date_created`)
+            VALUES(:note_id, :exam_question, :date_created)
+    '''
+    data = {
+        "note_id": args["note_id"], 
+        "exam_question": args["exam_question"],
+        "date_created": args["date_created"]
+    }
+    try:
+        id = DB.insert(qry, data)
+    except Exception:
+        print('Er is een probleem opgetreden, contact de admin.')
+
+    return {'message': 'success', 'id': id}, 201
+
+
+
+def create_question():
+    #ik pak open question uit de request in de frontend en voeg het toe in een var die ik daarna met een andere endpoint in de db 
+
+
+    if not request.is_json:
+        return {'message': 'Invalid request, JSON expected'}, 400
+
+    # Get the open question from the JSON request body
+    request_data = request.get_json()
+    open_question = request_data.get('open_question')
+
+   
+    api_key = os.getenv("API_KEY")
+    test_gpt = TestGPT(api_key)
+    generated_question = test_gpt.generate_open_question(note=open_question)
+
+
+    return {'message': 'success', 'generated_question': generated_question}, 201
+
+def get_notes(note_id):
+    qry = '''
+    SELECT notes.note_id , notes.note
+    FROM notes
     WHERE note_id = ?
+    
     '''
 
     try:
@@ -98,10 +239,15 @@ def get_specific_notes(note_id):
     return {'message': 'success', 'rows': rows}, 201
 
 
-def create_notes():
-    api_key = os.getenv("API_KEY")
-    test_gpt = TestGPT(api_key)
-    open_question = test_gpt.generate_open_question("de pc is oranje en de boek is blau en de tafel is zwart")
+def get_questions():
+    qry = '''
+     SELECT * FROM questions
+        '''
     
+    try:
+        rows = DB.all(qry)
+    except Exception:
+        print('Er is een probleem opgetreden, contact de admin.')
+    
+    return {'message': 'success', 'rows': rows}, 201
 
-    return {'message': 'success', 'open_question': open_question}, 201
